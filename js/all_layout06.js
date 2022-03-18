201eeefebe2c22582b59446cd9db2fa5c9e38ca8
nohsiwon <100886804+nohsiwon@users.noreply.github.com>
2022-03-18 16:32:12 +0900

    Add files via upload

--- js/all_layout06.js
+++ js/all_layout06.js
@@ -7,20 +7,67 @@ function topBannerHandler() {
   //  $('topBanner').hide() ; //display:none;
     $('.topBanner').slideUp();
 }
-$('.topBanner .container i').on('click', topBannerHandler)
+$('.topBanner .container i').on('click', topBannerHandler);
 
 
 
 
 $('.visualSlide').slick({
-    arrows: false,
-    autoplay:true,
-    autoplayspeed:2000,
+    arrows: true,
+    autoplay: true,
+    autoplayspeed: 3000,
     dots: true,
+    prevArrow: '<div class="msLeft"></div>',
+    nextArrow: '<div class="msRight"></div>'
 })
-///////////////////////////////
+
+
+//슬라이드에 애니메이션....
+$('.visualSlide figure').eq(1).addClass('oo');
+    $('.visualSlide').on('afterChange', function (e,s,c){
+        $('.visualSlide figure').eq(c+1).addClass('oo').siblings().removeClass('oo');
+        if (c===1) {
+            $('.msLeft').addClass('oo')
+        } else {
+            $('.msLeft').removeClass('oo')
+        }
+    });
+
+//
+
+$('.eProductSlide').slick({
+  arrows: false,
+  autoplay: true,
+  autoplayspeed:1000,
+})
+
+
+$('.eventProduct i:nth-of-type(1)').on('click', function(){
+  $('.eProductSlide').slick('slickPause')
+})
+
+$('.eventProduct i:nth-of-type(2)').on('click', function(){
+  $('.eProductSlide').slick('slickPlay')
+})
+
+
+//
+$('.aproductSlide').slick({
+  arrows:false,
+  dots:true,
+  slidesToShow:5,
+  centerMode: true,
+});
+
+$('.allProduct i:first-child').on('click', function(){
+  $('.aproductSlide').slick('slickPrev')
+});
+
+$('.allProduct i:last-child').on('click', function(){
+  $('.aproductSlide').slick('slickNext')
 });
 
 
 
-
+///////////////////////////////
+});
