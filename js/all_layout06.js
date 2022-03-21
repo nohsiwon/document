3bd61ea1566a8b9dd85f3aadc347b78872cac583
nohsiwon <100886804+nohsiwon@users.noreply.github.com>
2022-03-21 16:14:34 +0900

    Add files via upload

--- js/all_layout06.js
+++ js/all_layout06.js
@@ -1,73 +1,108 @@
-$(function(){
-///////////////////////////////
+$(function () {
+  ///////////////////////////////
 
-// topBanner : noh 2022.03.17 
+  // topBanner : noh 2022.03.17 
 
-function topBannerHandler() {
-  //  $('topBanner').hide() ; //display:none;
+  function topBannerHandler() {
+    //  $('topBanner').hide() ; //display:none;
     $('.topBanner').slideUp();
-}
-$('.topBanner .container i').on('click', topBannerHandler);
+  }
+  $('.topBanner .container i').on('click', topBannerHandler);
 
 
 
 
-$('.visualSlide').slick({
+  $('.visualSlide').slick({
     arrows: true,
     autoplay: true,
     autoplayspeed: 3000,
     dots: true,
     prevArrow: '<div class="msLeft"></div>',
     nextArrow: '<div class="msRight"></div>'
-})
+  })
 
 
-//슬라이드에 애니메이션....
-$('.visualSlide figure').eq(1).addClass('oo');
-    $('.visualSlide').on('afterChange', function (e,s,c){
-        $('.visualSlide figure').eq(c+1).addClass('oo').siblings().removeClass('oo');
-        if (c===1) {
-            $('.msLeft').addClass('oo')
-        } else {
-            $('.msLeft').removeClass('oo')
-        }
-    });
+  //슬라이드에 애니메이션....
+  $('.visualSlide figure').eq(1).addClass('oo');
+  $('.visualSlide').on('afterChange', function (e, s, c) {
+    $('.visualSlide figure').eq(c + 1).addClass('oo').siblings().removeClass('oo');
+    if (c === 1) {
+      $('.msLeft').addClass('oo')
+    } else {
+      $('.msLeft').removeClass('oo')
+    }
+  });
 
-//
+  //
 
-$('.eProductSlide').slick({
-  arrows: false,
-  autoplay: true,
-  autoplayspeed:1000,
-})
-
-
-$('.eventProduct i:nth-of-type(1)').on('click', function(){
-  $('.eProductSlide').slick('slickPause')
-})
-
-$('.eventProduct i:nth-of-type(2)').on('click', function(){
-  $('.eProductSlide').slick('slickPlay')
-})
+  $('.eProductSlide').slick({
+    arrows: false,
+    autoplay: true,
+    autoplayspeed: 1000,
+  })
 
 
-//
-$('.aproductSlide').slick({
-  arrows:false,
-  dots:true,
-  slidesToShow:5,
-  centerMode: true,
-});
+  $('.eventProduct i:nth-of-type(1)').on('click', function () {
+    $('.eProductSlide').slick('slickPause')
+  })
 
-$('.allProduct i:first-child').on('click', function(){
-  $('.aproductSlide').slick('slickPrev')
-});
-
-$('.allProduct i:last-child').on('click', function(){
-  $('.aproductSlide').slick('slickNext')
-});
+  $('.eventProduct i:nth-of-type(2)').on('click', function () {
+    $('.eProductSlide').slick('slickPlay')
+  })
 
 
+  //
+  $('.aproductSlide').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 5,
+    centerMode: true,
+  });
 
-///////////////////////////////
+  $('.allProduct i:first-child').on('click', function () {
+    $('.aproductSlide').slick('slickPrev')
+  });
+
+  $('.allProduct i:last-child').on('click', function () {
+    $('.aproductSlide').slick('slickNext')
+  });
+
+
+  //유튜브 불러오기
+
+
+  jQuery("#bgndVideo").YTPlayer({
+    videoURL: 'HUFsu-hegW8',
+    containment: '.movieBg',
+    autoPlay: true,
+    mute: true,
+    startAt: 0,
+    opacity: 1,
+    showControls: false,
+    playOnlyIfVisible: true,
+  });
+
+  $('.movieBg i:first-child').on('click', function () {
+    $("#bgndVideo").YTPPause()
+  });
+
+  $('.movieBg i:last-child').on('click', function () {
+    $("#bgndVideo").YTPPlay()
+  });
+
+  $('.tabMenu li').on('click', function () {
+    var idx = $(this).index();
+    $(this).addClass('oo').siblings().removeClass('oo');
+    $('.tabContent>div').eq(idx).addClass('oo').siblings().removeClass('oo')
+  });
+
+
+
+
+
+
+
+
+
+  ///////////////////////////////
 });
